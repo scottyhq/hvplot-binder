97721599b53e28b31d0234e8e7b98da4461b8135
Scott Henderson <scottyhq@gmail.com>
2021-07-11 17:55:12 -0700

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "machine urs.earthdata.nasa.gov login $EARTHDATA_USER password test" > .netrc
-
+ls -tlrha
+cat /home/jovyan/.netrc
 python -c "import netrc; (ASF_USER, account, ASF_PASS) = netrc.netrc().authenticators('urs.earthdata.nasa.gov'); print(ASF_USER, ASF_PASS)"
